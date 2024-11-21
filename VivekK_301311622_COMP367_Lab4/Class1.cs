b62f870080dac829ac796ea924b1b69356c40866
Vivek Kundaliya <vivek@trewac.com>
2024-11-21 18:13:34 -0500

    Initial Commit

---
+++ VivekK_301311622_COMP367_Lab4/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace VivekK_301311622_COMP367_Lab4
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ VivekK_301311622_COMP367_Lab4/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VivekK_301311622_COMP367_Lab4
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUppercase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return false;
+            return char.IsUpper(str[0]);
+        }
+
+    }
+}
